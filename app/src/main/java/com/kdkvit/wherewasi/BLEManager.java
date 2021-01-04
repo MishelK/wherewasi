15d9177a0b65ce03c1a56225b9be77c38023c0f8
Mishel Kvit <65822657+MishelK@users.noreply.github.com>
2021-01-04 13:35:03 +0200

    BLEManager created
    - Added BLEManager class which uses NordicSemi library for BLE scan, filter and callback

---
+++ app/src/main/java/com/kdkvit/wherewasi/BLEManager.java
@@ -0,0 +1,96 @@
+package com.kdkvit.wherewasi;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.nordicsemi.android.support.v18.scanner.BluetoothLeScannerCompat;
+import no.nordicsemi.android.support.v18.scanner.ScanCallback;
+import no.nordicsemi.android.support.v18.scanner.ScanFilter;
+import no.nordicsemi.android.support.v18.scanner.ScanResult;
+import no.nordicsemi.android.support.v18.scanner.ScanSettings;
+
+public class BLEManager {
+
+    private BluetoothLeScannerCompat scanner;
+    private boolean isScanning = false;
+
+    ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, @NonNull ScanResult result) {
+            super.onScanResult(callbackType, result);
+            Log.i("BLE", "Scan Result:");
+            String deviceName = result.getDevice().getName();
+            if (deviceName != null)
+                Log.i("BLE", deviceName);
+            else
+                Log.i("BLE", "ERROR: device name null");
+
+            String toString = result.toString();
+            if (toString != null)
+                Log.i("BLE", toString);
+            else
+                Log.i("BLE", "ERROR: toString null");
+        }
+
+        @Override
+        public void onBatchScanResults(@NonNull List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            for (int i = 0; i < results.size(); i++) {
+                Log.i("BLE", "Scan Result:");
+                String deviceName = results.get(i).getDevice().getName();
+                if (deviceName != null)
+                    Log.i("BLE", deviceName);
+                else
+                    Log.i("BLE", "ERROR: device name null");
+
+                String toString = results.get(i).toString();
+                if (toString != null)
+                    Log.i("BLE", toString);
+                else
+                    Log.i("BLE", "ERROR: toString null");
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.i("BLE","scan failed");
+        }
+    };
+
+    public BLEManager() {
+    }
+
+    public void startScan(){
+        isScanning = true;
+
+        scanner = BluetoothLeScannerCompat.getScanner();
+        ScanSettings settings = new ScanSettings.Builder()
+                .setLegacy(false)
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .setReportDelay(1000)
+                .setUseHardwareBatchingIfSupported(true)
+                .build();
+        List<ScanFilter> filters = new ArrayList<>();
+        filters.add(new ScanFilter.Builder().build());
+        scanner.startScan(filters, settings, scanCallback);
+        Log.i("BLE","Start Scan");
+    }
+
+    public void stopScan() {
+        isScanning = false;
+
+        BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
+        scanner.stopScan(scanCallback);
+        Log.i("BLE","Stop Scan");
+    }
+
+    public boolean isScanning() {
+        return isScanning;
+    }
+
+}
--- app/src/main/java/com/kdkvit/wherewasi/MainActivity.java
+++ app/src/main/java/com/kdkvit/wherewasi/MainActivity.java
@@ -1,14 +1,45 @@
 package com.kdkvit.wherewasi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.nordicsemi.android.support.v18.scanner.BluetoothLeScannerCompat;
+import no.nordicsemi.android.support.v18.scanner.ScanCallback;
+import no.nordicsemi.android.support.v18.scanner.ScanFilter;
+import no.nordicsemi.android.support.v18.scanner.ScanResult;
+import no.nordicsemi.android.support.v18.scanner.ScanSettings;
 
 public class MainActivity extends AppCompatActivity {
+
+    BLEManager bleManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bleManager = new BLEManager();
+
+        Button btnScan = findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bleManager.isScanning()){
+                    bleManager.stopScan();
+                }
+                else {
+                    bleManager.startScan();
+                }
+            }
+        });
     }
+
 }
