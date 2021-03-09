958299953bbf85e4398ff879a10dd747c2a7c568
Kobi Dalal <kobidalal1@gmail.com>
2021-03-09 23:30:46 +0200

    added welcome screen

---
+++ app/src/main/java/com/kdkvit/wherewasi/MainActivity.java
@@ -0,0 +1,43 @@
+package com.kdkvit.wherewasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kdkvit.wherewasi.fragments.MainFragment;
+import com.kdkvit.wherewasi.fragments.WelcomeFragment;
+import com.kdkvit.wherewasi.utils.SharedPreferencesUtils;
+
+import java.util.UUID;
+
+import models.User;
+
+import static actions.actions.sendUserToBe;
+
+public class MainActivity extends AppCompatActivity {
+
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Init user from local storage
+        user = SharedPreferencesUtils.getUser(this);
+        if(user==null){ //User not exists -> welcome page
+            WelcomeFragment welcomeFragment = new WelcomeFragment(name -> {
+                user = new User(name, UUID.randomUUID().toString());
+                sendUserToBe(MainActivity.this,user);
+                MainFragment mainFragment = new MainFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, mainFragment).commit();
+            });
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, welcomeFragment).commit();
+        }else{ //User exists -> Main Page
+            if(user.getId() == 0) { //User want not created in DB
+                sendUserToBe(this, user);
+            }
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container, mainFragment).commit();
+        }
+    }
+}
