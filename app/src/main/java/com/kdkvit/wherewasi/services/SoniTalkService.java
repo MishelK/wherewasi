2b30597d70402903283ac3a295b7592b5b9125e4
Kobi Dalal <kobidalal1@gmail.com>
2021-05-19 21:58:07 +0300

    init service

--- app/src/main/java/com/kdkvit/wherewasi/services/SoniTalkService.java
+++ app/src/main/java/com/kdkvit/wherewasi/services/SoniTalkService.java
@@ -1,22 +1,62 @@
 package com.kdkvit.wherewasi.services;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-public class SoniTalkService extends Service {
+import com.kdkvit.wherewasi.R;
+import com.kdkvit.wherewasi.sonitalk.SoniTalkContext;
+import com.kdkvit.wherewasi.sonitalk.SoniTalkMessage;
+import com.kdkvit.wherewasi.sonitalk.SoniTalkPermissionsResultReceiver;
+import com.kdkvit.wherewasi.sonitalk.SoniTalkSender;
+import com.kdkvit.wherewasi.sonitalk.utils.ConfigConstants;
+
+import static android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
+
+public class SoniTalkService extends Service implements SoniTalkPermissionsResultReceiver.Receiver {
+    private SoniTalkContext soniTalkContext;
+    private SoniTalkSender soniTalkSender;
+    private SoniTalkPermissionsResultReceiver soniTalkPermissionsResultReceiver;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+    private Toast currentToast;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 42;
+    private String [] PERMISSIONS = {Manifest.permission.RECORD_AUDIO};
 
+    // These request codes are used to know which action was accepted / denied by the user.
+    public static final int ON_SENDING_REQUEST_CODE = 2001;
+    public static final int ON_RECEIVING_REQUEST_CODE = 2002;
+    private SharedPreferences sp;
+    private SoniTalkMessage currentMessage;
 
     @Override
     public void onCreate() { //Called once when service is instantiated
         super.onCreate();
+
+        soniTalkPermissionsResultReceiver = new SoniTalkPermissionsResultReceiver(new Handler());
+        soniTalkPermissionsResultReceiver.setReceiver(this); // To receive callbacks from the SDK
+        soniTalkContext = SoniTalkContext.getInstance(this, soniTalkPermissionsResultReceiver);
+        if (soniTalkContext == null) {
+            soniTalkContext = SoniTalkContext.getInstance(this, soniTalkPermissionsResultReceiver);
+        }
     }
 
     @Override
@@ -25,7 +65,7 @@ public class SoniTalkService extends Service {
         if (command != null) {
             switch (command) {
                 case "start":
-                    //startScan();
+                    startSending();
                     break;
                 case "stop":
                     //stopScan();
@@ -35,6 +75,170 @@ public class SoniTalkService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
+    public void startSending(){
+
+    }
 
 
+    public void sendMessage(){
+        //Log.d("PlayClick","I got clicked");
+
+            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            int currentVolume = audioManager.getStreamVolume(3);
+            sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            SharedPreferences.Editor ed = sp.edit();
+            ed.putInt(ConfigConstants.CURRENT_VOLUME, currentVolume);
+            ed.apply();
+
+            int volume = Integer.valueOf(sp.getString(ConfigConstants.LOUDNESS, ConfigConstants.SETTING_LOUDNESS_DEFAULT));
+            audioManager.setStreamVolume(3, (int) Math.round((audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * volume/100.0D)), 0);
+
+            if (soniTalkContext == null) {
+                soniTalkContext = SoniTalkContext.getInstance(this, soniTalkPermissionsResultReceiver);
+            }
+            soniTalkSender = soniTalkContext.getSender();
+            soniTalkSender.send(currentMessage, ON_SENDING_REQUEST_CODE);
+
+//        } else{
+//            Toast.makeText(getApplicationContext(), getString(R.string.signal_generator_not_created),Toast.LENGTH_LONG).show();
+//        }
+    }
+
+    @Override
+    public void onSoniTalkPermissionResult(int resultCode, Bundle resultData) {
+        int actionCode = 0;
+        if(resultData != null){
+            actionCode = resultData.getInt(getString(R.string.bundleRequestCode_key));
+        }
+        switch (resultCode) {
+            case SoniTalkContext.ON_PERMISSION_LEVEL_DECLINED:
+                //Log.d(TAG, "onSoniTalkPermissionResult ON_PERMISSION_LEVEL_DECLINED");
+                //Log.d(TAG, String.valueOf(resultData.getInt(getString(R.string.bundleRequestCode_key), 0)));
+
+                if (currentToast != null) {
+                    currentToast.cancel();
+                }
+                switch (actionCode) {
+                    case ON_RECEIVING_REQUEST_CODE:
+                        currentToast = Toast.makeText(this, getString(R.string.on_receiving_listening_permission_required), Toast.LENGTH_SHORT);
+                        currentToast.show();
+                        //onButtonStopListening();
+
+                        // Set buttons in the state NOT RECEIVING
+
+                        break;
+                    case ON_SENDING_REQUEST_CODE:
+                        currentToast = Toast.makeText(this, getString(R.string.on_sending_sending_permission_required), Toast.LENGTH_SHORT);
+                        currentToast.show();
+
+                        // Set buttons in the state NOT RECEIVING
+
+                        break;
+                }
+
+
+                break;
+            case SoniTalkContext.ON_REQUEST_GRANTED:
+                //Log.d(TAG, "ON_REQUEST_GRANTED");
+                //Log.d(TAG, String.valueOf(resultData.getInt(getString(R.string.bundleRequestCode_key), 0)));
+
+                switch (actionCode){
+                    case ON_RECEIVING_REQUEST_CODE:
+                        // Set buttons in the state RECEIVING
+
+                        //setReceivedText(getString(R.string.decoder_start_text));
+                        break;
+
+                    case ON_SENDING_REQUEST_CODE:
+
+                        //saveButtonStates();
+                        break;
+                }
+
+                break;
+            case SoniTalkContext.ON_REQUEST_DENIED:
+                //Log.d(TAG, "ON_REQUEST_DENIED");
+                //Log.d(TAG, String.valueOf(resultData.getInt(getString(R.string.bundleRequestCode_key), 0)));
+
+                if (currentToast != null) {
+                    currentToast.cancel();
+                }
+
+                // Checks the requestCode to adapt the UI depending on the action type (receiving or sending)
+                switch (actionCode) {
+                    case ON_RECEIVING_REQUEST_CODE:
+                        //showRequestPermissionExplanation(R.string.on_receiving_listening_permission_required);
+                        // Set buttons in the state NOT RECEIVING
+                        currentToast = Toast.makeText(this, getString(R.string.on_receiving_listening_permission_required), Toast.LENGTH_SHORT);
+                        currentToast.show();
+                        //saveButtonState(ConfigConstants.LISTEN_BUTTON_ENABLED,true);
+                        //btnListen.setImageResource(R.drawable.baseline_hearing_grey_48);
+
+                        break;
+                    case ON_SENDING_REQUEST_CODE:
+                        //showRequestPermissionExplanation(R.string.on_sending_sending_permission_required);
+                        currentToast = Toast.makeText(this, getString(R.string.on_sending_sending_permission_required), Toast.LENGTH_SHORT);
+                        currentToast.show();
+                        //saveButtonState(ConfigConstants.SEND_BUTTON_ENABLED,true);
+                        //btnPlay.setImageResource(R.drawable.ic_volume_up_grey_48dp);
+
+                }
+                break;
+
+            case SoniTalkContext.ON_SEND_JOB_FINISHED:
+                ///saveButtonState(ConfigConstants.SEND_BUTTON_ENABLED,true);
+                //btnPlay.setImageResource(R.drawable.ic_volume_up_grey_48dp);
+                sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                int currentVolume = sp.getInt(ConfigConstants.CURRENT_VOLUME, ConfigConstants.CURRENT_VOLUME_DEFAULT);
+                AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setStreamVolume(3, currentVolume, 0);
+
+                break;
+
+            case SoniTalkContext.ON_SHOULD_SHOW_RATIONALE_FOR_ALLOW_ALWAYS:
+                /*if (currentToast != null) {
+                    currentToast.cancel();
+                }
+                currentToast = Toast.makeText(MainActivity.this, "Choosing Allow always requires you to accept the Android permission", Toast.LENGTH_LONG);
+                currentToast.show();*/
+                break;
+
+            case SoniTalkContext.ON_REQUEST_L0_DENIED:
+                //Log.d(TAG, "ON_REQUEST_L0_DENIED");
+                switch (actionCode) {
+                    case ON_RECEIVING_REQUEST_CODE:
+                        showRequestPermissionExplanation(R.string.on_receiving_listening_permission_required);
+
+                        break;
+                    case ON_SENDING_REQUEST_CODE:
+                        showRequestPermissionExplanation(R.string.on_sending_sending_permission_required);
+
+
+                }
+                break;
+
+            default:
+
+                break;
+
+        }
+    }
+
+    private void showRequestPermissionExplanation(int messageId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(messageId);
+        builder.setPositiveButton(R.string.permission_request_explanation_positive,new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent();
+                        intent.setAction(ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        startActivity(intent);
+                    }
+                }
+        );
+        builder.setNegativeButton(R.string.permission_request_explanation_negative, null);
+        builder.show();
+    }
 }
--- app/src/main/java/com/kdkvit/wherewasi/sonitalk/utils/ConfigConstants.java
+++ app/src/main/java/com/kdkvit/wherewasi/sonitalk/utils/ConfigConstants.java
@@ -30,12 +30,12 @@ public class ConfigConstants {
      * The following String values are used as keys for the json configuration files.
      * Please do not modify, or if you do, keep everything in sync.
      */
-    public static final String FREQUENCY_ZERO = "frequency-zero"; // Lowest frequency used in Hz
-    public static final String BIT_PERIOD = "bit-period"; // Bit duration in ms
-    public static final String PAUSE_PERIOD = "pause-period"; // Pause duration in ms
-    public static final String NUMBER_OF_MESSAGE_BLOCKS = "number-of-message-blocks";
-    public static final String NUMBER_OF_FREQUENCIES = "number-of-frequencies";
-    public static final String SPACE_BETWEEN_FREQUENCIES = "space-between-frequencies"; // In Hz
+//    public static final String FREQUENCY_ZERO = "frequency-zero"; // Lowest frequency used in Hz
+//    public static final String BIT_PERIOD = "bit-period"; // Bit duration in ms
+//    public static final String PAUSE_PERIOD = "pause-period"; // Pause duration in ms
+//    public static final String NUMBER_OF_MESSAGE_BLOCKS = "number-of-message-blocks";
+//    public static final String NUMBER_OF_FREQUENCIES = "number-of-frequencies";
+//    public static final String SPACE_BETWEEN_FREQUENCIES = "space-between-frequencies"; // In Hz
     /*** ***/
 
     /* NOT used in this version.
@@ -51,4 +51,46 @@ public class ConfigConstants {
 */
     public static final byte[] GENERATOR_POLYNOM = new byte[] { 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1}; //CRC-17-CAN : 0x1685B
     public static final String CONTROL_FILLING_CHARACTER = "00011001";
+    public static final String FREQUENCY_ZERO = "etprefFrequencyZero";
+    public static final String BIT_PERIOD = "etprefBitperiod";
+    public static final String PAUSE_PERIOD = "etprefPauseperiod";
+    public static final String NUMBER_OF_FREQUENCIES = "lpprefNFrequencies";
+    public static final String SPACE_BETWEEN_FREQUENCIES = "etprefFrequencyspace";
+    public static final String NUMBER_OF_BYTES = "etprefNMaxBytes";
+    public static final String LOUDNESS = "sbprefLoudness";
+    public static final String PRESET = "setPresetPreferences";
+
+    public static final String SETTING_FREQUENCY_ZERO_DEFAULT = "18000";
+    public static final String SETTING_BIT_PERIOD_DEFAULT = "100";
+    public static final String SETTING_PAUSE_PERIOD_DEFAULT = "0";
+    public static final String SETTING_NUMBER_OF_FREQUENCIES_DEFAULT = "16";
+    public static final String SETTING_SPACE_BETWEEN_FREQUENCIES_DEFAULT = "100";
+    public static final String SETTING_NUMBER_OF_BYTES_DEFAULT = "18";
+    public static final String SETTING_LOUDNESS_DEFAULT = "70";
+
+
+    public static final int SETTING_BIT_PERIOD_LOWER_LIMIT = 30;
+    public static final int SETTING_BIT_PERIOD_UPPER_LIMIT = 200;
+    public static final int SETTING_PAUSE_PERIOD_LOWER_LIMIT = 0;
+    public static final int SETTING_PAUSE_PERIOD_UPPER_LIMIT = 200;
+    public static final int SETTING_FREQUENCY_ZERO_LOWER_LIMIT = 50;
+    public static final int SETTING_FREQUENCY_ZERO_UPPER_LIMIT = 20000;
+    public static final int SETTING_NUMBER_OF_BYTES_LOWER_LIMIT = 2;
+    public static final int SETTING_NUMBER_OF_BYTES_UPPER_LIMIT = 30;
+    public static final int SETTING_SPACE_BETWEEN_FREQUENCIES_LOWER_LIMIT = 50;
+    public static final int SETTING_SPACE_BETWEEN_FREQUENCIES_UPPER_LIMIT = 200;
+    public static final int SETTING_LOUDNESS_LOWER_LIMIT = 1;
+    public static final int SETTING_LOUDNESS_UPPER_LIMIT = 100;
+
+    public static final String PREFERENCE_RESET_PREFERENCES = "resetPreferences";
+    public static final String PREFERENCE_PRESET_PREFERENCES = "setPresetPreferences";
+
+    public static final String CURRENT_VOLUME = "current-volume";
+    public static final int CURRENT_VOLUME_DEFAULT = 70;
+
+    public static final String SEND_BUTTON_ENABLED = "send-button-enabled";
+    public static final String LISTEN_BUTTON_ENABLED = "listen-button-enabled";
+    public static final String STOP_LISTEN_BUTTON_ENABLED = "stop-listen-button-enabled";
+
+    public static final String TEXT_TO_SEND = "text-to-send";
 }
